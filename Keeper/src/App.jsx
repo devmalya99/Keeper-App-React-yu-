4a3683d51a5eac2259c8046f475f44e73e7f686c
devmalya99 <debmalyamazumdar99@gmail.com>
2024-01-17 14:41:37 +0530

    first commit

---
+++ Keeper/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from "./Components/Header";
+import Footer from './Components/Footer';
+function App() {
+
+  return (
+    <>
+      <Header />
+      <br></br>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
