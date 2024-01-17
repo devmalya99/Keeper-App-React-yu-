ad684096c4ae8da0c0e9d71f34f5fc56cc3fa808
devmalya99 <debmalyamazumdar99@gmail.com>
2024-01-17 17:08:10 +0530

    Keeper app stage 1 done

--- Keeper/src/App.jsx
+++ Keeper/src/App.jsx
@@ -2,12 +2,20 @@
 import './App.css'
 import Header from "./Components/Header";
 import Footer from './Components/Footer';
+
+import Note from "./Components/Note"
 function App() {
 
   return (
     <>
       <Header />
+      
       <br></br>
+      
+        <Note />
+      <Note/>
+      
+      
       <Footer/>
     </>
   )
